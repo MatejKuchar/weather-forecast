72c6455969e087ba83a8fef53aeb9675eb541769
MatejKuchar <matej.kuchar01@gmail.com>
2021-01-28 23:46:37 +0100

    add styles from html to css file and change loader icon

--- script.js
+++ script.js
@@ -1,56 +1,58 @@
-const form = document.getElementById('form');
-const input = document.getElementById('forecastInput');
-const button = document.getElementById('button');
-const message = document.getElementById('message');
-const warning = document.querySelector('.warning');
+const form = document.getElementById("form");
+const input = document.getElementById("forecastInput");
+const button = document.getElementById("button");
+const message = document.getElementById("message");
+const warning = document.querySelector(".warning");
 
-const apiKey = '3dc2782704c76b46551d64b37c782440';
+const apiKey = "3dc2782704c76b46551d64b37c782440";
 
-form.addEventListener('submit', getFormInput);
-form.addEventListener('keyup', function(){
-    document.getElementById('watherInfo').innerHTML = "";
-
+form.addEventListener("submit", getFormInput);
+form.addEventListener("keyup", function () {
+  document.getElementById("watherInfo").innerHTML = "";
 });
 
-function getFormInput(e){
-    const loader = document.createElement('img');
-    loader.setAttribute('src', 'loader.gif');
-   
-    document.querySelector('.loader-img').appendChild(loader);
+function getFormInput(e) {
+  const loader = document.createElement("img");
+  loader.setAttribute(
+    "src",
+    "https://upload.wikimedia.org/wikipedia/commons/2/29/Loader.gif"
+  );
 
-    if(!input.value.length){
-        document.querySelector('.loader-img').removeChild(loader);
-        let warningMessage =    `<div class="alert alert-danger mt-3" role="alert">
+  document.querySelector(".loader-img").appendChild(loader);
+
+  if (!input.value.length) {
+    document.querySelector(".loader-img").removeChild(loader);
+    let warningMessage = `<div class="alert alert-danger mt-3" role="alert">
                                 Input is empty!
                                 </div>`;
-        warning.innerHTML = warningMessage;
-        setTimeout(() => {
-            warning.innerHTML = '';
-        }, 2500)
-    } else {
-        let location = input.value;
-        setTimeout(() => {
-            document.querySelector('.loader-img').removeChild(loader);
-            getWeather(location);
-        }, 1500);
-    }
+    warning.innerHTML = warningMessage;
+    setTimeout(() => {
+      warning.innerHTML = "";
+    }, 2500);
+  } else {
+    let location = input.value;
+    setTimeout(() => {
+      document.querySelector(".loader-img").removeChild(loader);
+      getWeather(location);
+    }, 1500);
+  }
 
-    e.preventDefault();
+  e.preventDefault();
 }
 
+async function getWeather(location) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`
+    );
+    const data = await response.json();
 
-async function getWeather(location){
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`);
-        const data = await response.json();
+    let { name, main, weather } = data;
+    let { country } = data.sys;
+    let { temp, humidity } = main;
+    let [type] = weather;
 
-        let {name, main, weather} = data;
-        let {country} = data.sys;
-        let {temp, humidity} = main;
-        let [type] = weather;
-
-        let weatherData = 
-                            `
+    let weatherData = `
                             <div class="card text-center mt-3">
                                 <div class="card-header">
                                     
@@ -68,11 +70,9 @@ async function getWeather(location){
                             </div>
                             `;
 
-        document.getElementById('watherInfo').innerHTML = weatherData;
-        input.value = '';
-
-    } catch(e){
-        console.log(e);
-    }
-
+    document.getElementById("watherInfo").innerHTML = weatherData;
+    input.value = "";
+  } catch (e) {
+    console.log(e);
+  }
 }
